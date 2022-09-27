a2678d5069f8479d3720a2d475024c3ab13058a3
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-27 18:12:32 +0100

    refactor: create puzzle runner function

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import importlib
 import sys
 
-if __name__ == '__main__':
-    day = sys.argv[1]
+
+def run_puzzle(day):
     run = importlib.import_module(f'advent_of_code_2021.{day}.run')
     run.run()
+
+
+if __name__ == '__main__':
+    day = sys.argv[1]
+    run_puzzle(day)
