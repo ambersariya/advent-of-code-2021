cbf126a73a2ba6f4ade63286d5ba198461f1dd6a
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-28 12:41:11 +0100

    test(pong): add testcase for getting gamma & epsilon rate

--- tests/unit/day03/test_diagnostics_service.py
+++ tests/unit/day03/test_diagnostics_service.py
@@ -11,3 +11,11 @@ def test_throw_error_when_not_enough_data_passed_in():
     with pytest.raises(NotEnoughRawData):
         diagnostics_service.generate_report(raw_report=raw_report)
 
+
+def test_should_return_diagnostic_report():
+    raw_report = ['00100']
+    diagnostics_service = DiagnosticsService()
+    report = diagnostics_service.generate_report(raw_report=raw_report)
+
+    assert report.gamma_rate == 4
+    assert report.epsilon_rate == 27
