563b0aa11bc6cfaf61281a9966feb123e942d978
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-28 12:32:12 +0100

    test: pass testcase for not enough data

--- advent_of_code_2021/day03/diagnostics_service.py
+++ advent_of_code_2021/day03/diagnostics_service.py
@@ -1,6 +1,8 @@
 from advent_of_code_2021.day03.diagnostics_report import DiagnosticsReport
+from advent_of_code_2021.day03.error import NotEnoughRawData
 
 
 class DiagnosticsService:
     def generate_report(self, raw_report: list[str]) -> DiagnosticsReport:
-        raise NotImplementedError()
+        if len(raw_report) == 0:
+            raise NotEnoughRawData()
