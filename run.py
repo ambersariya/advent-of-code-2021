66fecb33fc8080eec219449666947f0580cbe6e5
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-28 11:06:16 +0100

    refactor: print error message when we don't puzzle for a given day

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ import sys
 
 
 def run_puzzle(day):
-    run = importlib.import_module(f'advent_of_code_2021.{day}.run')
-    run.run()
+    try:
+        run = importlib.import_module(f'advent_of_code_2021.{day}.run')
+        run.run()
+    except:
+        print(f"ERROR: {day} does not exist")
 
 
 if __name__ == '__main__':
