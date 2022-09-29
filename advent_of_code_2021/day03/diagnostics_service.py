1be0830f103a0f37e5b9f1df78a7cfde36f99786
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-29 15:24:15 +0100

    test: all tests implemented for diagnostics service

--- advent_of_code_2021/day03/diagnostics_service.py
+++ advent_of_code_2021/day03/diagnostics_service.py
@@ -8,22 +8,28 @@ def column(matrix, i):
 
 class DiagnosticsService:
     def generate_report(self, raw_report: list[str]) -> DiagnosticsReport:
-        if len(raw_report) == 0:
-            raise NotEnoughRawData()
-        report_matrix = list(map(lambda entry: list(entry), raw_report))
-        binary_gamma = ''
-        for index in range(0, len(report_matrix[0])):
-            columnar_bits = column(report_matrix, index)
-            num_of_zeros = self.__num_of_zeros(columnar_bits)
-            total_bits = len(columnar_bits)
-            num_of_ones = total_bits - num_of_zeros
-            binary_gamma += ('0' if num_of_zeros > num_of_ones else '1')
+        binary_gamma = self.binary_gamma(raw_report)
         binary_epsilon = self.__flip_bits(binary_gamma)
 
         return DiagnosticsReport(
             gamma_rate=int(binary_gamma, base=2),
-            epsilon_rate=int(binary_epsilon, base=2)
+            epsilon_rate=int(binary_epsilon, base=2),
+            oxygen_generator_rate=self.generate_oxygen_rating(raw_report=raw_report),
+            co2_scrubber_rate=self.co2_scrubber_rating(raw_report=raw_report)
         )
+
+    def binary_gamma(self, raw_report) -> str:
+        if len(raw_report) == 0:
+            raise NotEnoughRawData()
+        report_matrix = list(map(lambda entry: list(entry), raw_report))
+        bin_gamma = ''
+        for index in range(0, len(report_matrix[0])):
+            columnar_bits = column(report_matrix, index)
+            total_bits = len(columnar_bits)
+            num_of_zeros = self.__num_of_zeros(columnar_bits)
+            num_of_ones = total_bits - num_of_zeros
+            bin_gamma += ('0' if num_of_zeros > num_of_ones else '1')
+        return bin_gamma
 
     @staticmethod
     def __flip_bits(bits: str):
@@ -32,3 +38,49 @@ class DiagnosticsService:
     @staticmethod
     def __num_of_zeros(bits: list):
         return len(list(filter(lambda _bit: _bit == '0', bits)))
+
+    def generate_oxygen_rating(self, raw_report: list[str]) -> int:
+        if len(raw_report) == 1:
+            return int(raw_report[0], 2)
+        report_matrix = list(map(lambda entry: list(entry), raw_report))
+        bit_column_index = 0
+        while len(report_matrix) > 1:
+            most_common = []
+            column_values = column(report_matrix, bit_column_index)
+            common_bit = self.most_common_bit(bits=column_values)
+            for reading in report_matrix:
+                if reading[bit_column_index] == common_bit:
+                    most_common.append(reading)
+            bit_column_index += 1
+            report_matrix = most_common
+        return int("".join(report_matrix[0]), 2)
+
+    def co2_scrubber_rating(self, raw_report: list[str]) -> int:
+        if len(raw_report) == 1:
+            return int(raw_report[0], 2)
+        report_matrix = list(map(lambda entry: list(entry), raw_report))
+        bit_column_index = 0
+        while len(report_matrix) > 1:
+            most_common = []
+            column_values = column(report_matrix, bit_column_index)
+            common_bit = self.least_common_bit(bits=column_values)
+            for reading in report_matrix:
+                if reading[bit_column_index] == common_bit:
+                    most_common.append(reading)
+            bit_column_index += 1
+            report_matrix = most_common
+        return int("".join(report_matrix[0]), 2)
+
+    @staticmethod
+    def most_common_bit(bits: list):
+        starts_with_one = list(filter(lambda binary: binary == '1', bits))
+        num_of_ones = len(starts_with_one)
+        num_of_zeros = len(bits) - num_of_ones
+        return "1" if num_of_ones >= num_of_zeros else "0"
+
+    @staticmethod
+    def least_common_bit(bits: list):
+        starts_with_one = list(filter(lambda binary: binary == '1', bits))
+        num_of_ones = len(starts_with_one)
+        num_of_zeros = len(bits) - num_of_ones
+        return "0" if num_of_zeros <= num_of_ones else "1"
--- tests/unit/day03/test_diagnostics_service.py
+++ tests/unit/day03/test_diagnostics_service.py
@@ -24,6 +24,7 @@ def test_should_return_diagnostic_report_with_one_raw_reading():
     assert report.power_consumption == 108
     assert report.oxygen_generator_rate == 4
     assert report.co2_scrubber_rate == 4
+    assert report.life_support_rate == 16
 
 
 def test_should_return_diagnostic_report_from_multiple_raw_readings():
@@ -36,3 +37,40 @@ def test_should_return_diagnostic_report_from_multiple_raw_readings():
     assert report.power_consumption == 210
     assert report.oxygen_generator_rate == 21
     assert report.co2_scrubber_rate == 4
+    assert report.life_support_rate == 84
+
+
+def test_oxygen_generator_rating():
+    raw_report = ["00100",
+                  "11110",
+                  "10110",
+                  "10111",
+                  "10101",
+                  "01111",
+                  "00111",
+                  "11100",
+                  "10000",
+                  "11001",
+                  "00010",
+                  "01010"]
+    diagnostics_service = DiagnosticsService()
+    report = diagnostics_service.generate_oxygen_rating(raw_report=raw_report)
+    assert report == 23
+
+
+def test_co2_scrubber_rating():
+    raw_report = ["00100",
+                  "11110",
+                  "10110",
+                  "10111",
+                  "10101",
+                  "01111",
+                  "00111",
+                  "11100",
+                  "10000",
+                  "11001",
+                  "00010",
+                  "01010"]
+    diagnostics_service = DiagnosticsService()
+    report = diagnostics_service.co2_scrubber_rating(raw_report=raw_report)
+    assert report == 10
