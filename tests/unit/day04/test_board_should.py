0788fa1256cc3375b3b6228130258e3fa9b5eedd
iagardi <istvan.agardi83@gmail.com>
2022-09-29 16:28:21 +0100

    green test to parse raw string into a board

--- tests/unit/day04/test_board_should.py
+++ tests/unit/day04/test_board_should.py
@@ -1,6 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass(init=True, frozen=True)
 class Board:
-    def parse(self, raw_str: str) -> "Board":
-        raise NotImplementedError()
+    board: list
+
+    @staticmethod
+    def parse(raw_str: str) -> "Board":
+        board = []
+        raw_str = raw_str.strip("\n")
+        generate_rows = raw_str.split("\n")
+        for row in generate_rows:
+            row = list(filter(lambda r: len(r) > 0, row.split(" ")))
+            board.append(row)
+        return Board(board=board)
+
+    @property
+    def rows(self) -> int:
+        return len(self.board)
+
+    @property
+    def columns(self) -> int:
+        return len(self.board[0])
+
+    def number_at(self, row: int, column: int) -> int:
+        return int(self.board[row][column])
 
 
 def test_generate_a_board_from_string():
@@ -11,11 +35,9 @@ def test_generate_a_board_from_string():
 6 10  3 18  5
 1 12 20 15 19
 '''
-    board_parser = Board()
-    game_board = board_parser.parse(raw_str=raw_str)
+    game_board = Board.parse(raw_str=raw_str)
 
     assert game_board.rows == 5
     assert game_board.columns == 5
     assert game_board.number_at(column=1, row=0) == 13
     assert game_board.number_at(column=2, row=2) == 14
-
