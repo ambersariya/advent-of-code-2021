d1002937e4efc86dcdb66053c8666ddfd6f48558
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-29 15:28:32 +0100

    chore: add day04 description & input

--- advent_of_code_2021/day03/run.py
+++ advent_of_code_2021/day03/run.py
@@ -1,3 +1,5 @@
+import string
+
 from advent_of_code_2021.day03.diagnostics_service import DiagnosticsService
 
 PUZZLE_INPUT = 'puzzles/day03.txt'
@@ -13,4 +15,11 @@ def run():
     puzzle_input = read_file(filepath=PUZZLE_INPUT)
     service = DiagnosticsService()
     report = service.generate_report(raw_report=puzzle_input)
-    print(report.power_consumption)
+    template = string.Template("""
+    power_consumption=$power_consumption
+    life_support_rate=$life_support_rate
+    """)
+    print(template.safe_substitute(
+        power_consumption=report.power_consumption,
+        life_support_rate=report.life_support_rate
+    ))
